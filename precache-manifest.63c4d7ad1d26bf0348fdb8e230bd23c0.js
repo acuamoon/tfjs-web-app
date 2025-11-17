551dec44febe2fa8a689b373f872b5b1e85c5620
unknown <acuamoon@hanmail.net>
2025-11-17 13:19:12 +0900

    Deploy to GitHub Pages

---
+++ precache-manifest.63c4d7ad1d26bf0348fdb8e230bd23c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e579721251f676d5547984da0e13eb90",
+    "url": "/tfjs-web-app/index.html"
+  },
+  {
+    "revision": "bd961ef8281ada6298c7",
+    "url": "/tfjs-web-app/static/css/2.6efdf584.chunk.css"
+  },
+  {
+    "revision": "4c15a41e8e48f6e1c98e",
+    "url": "/tfjs-web-app/static/css/main.441d783a.chunk.css"
+  },
+  {
+    "revision": "bd961ef8281ada6298c7",
+    "url": "/tfjs-web-app/static/js/2.cd3c1c56.chunk.js"
+  },
+  {
+    "revision": "4c15a41e8e48f6e1c98e",
+    "url": "/tfjs-web-app/static/js/main.edbc8538.chunk.js"
+  },
+  {
+    "revision": "e0fbe2fa30db9002b896",
+    "url": "/tfjs-web-app/static/js/runtime~main.12d82480.js"
+  }
+]);
